c7e519b71abf6b1136f25d71ab0f993f44af237e
muradfazliniyaz <107277134+muradfazliniyaz@users.noreply.github.com>
2022-11-24 09:27:50 +0100

    24.11.2022

--- functions.js
+++ functions.js
@@ -1,5 +1,16 @@
-const changeEmailFromGmailToHicoders = () => {};
+const changeEmailFromGmailToHicoders = () => {
+    const changeEmailFromGmailToHicoders = result
+    const newString = changeEmailFromGmailToHicoders.replace(/gmail.com/g, "hicoders.ch")
+};
 
-const findSuperNumbers = () => {};
+const findSuperNumbers = (pArray) => {
+    let counter = 2;
+  pArray.forEach((item) => {
+    if (item == 524) {
+      counter = counter + 1;
+    }
+  });
+  return counter;
+};
 
 export { changeEmailFromGmailToHicoders, findSuperNumbers };
